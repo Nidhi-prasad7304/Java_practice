c3c49e8d1251c00e05f0b7910145029fa06f7d95
Nidhi-prasad7304 <nidhiprasad7304@gmail.com>
2025-04-15 23:50:37 +0530

    sum of two number

---
+++ 1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class a
+{
+    public static void main(String args[])
+    {
+        Scanner input=new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Enter the value of a");
+        a=input.nextInt();
+        System.out.println("Enter the value of b");
+        b=input.nextInt();
+        System.out.println("Value of a :"+a);
+        System.out.println("Value of b:"+b);
+        c=a+b;
+        System.out.println("total :"+c );
+    }
+}
